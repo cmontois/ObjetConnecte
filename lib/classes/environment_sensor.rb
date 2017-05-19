4058996bfec9c48d4a7f07a797f46276513ed524
Clément <clement.montois@ig2i.fr>
2017-05-19 20:43:41 +0200

    -> Trying to print value

--- lib/classes/environment_sensor.rb
+++ lib/classes/environment_sensor.rb
@@ -1,7 +1,7 @@
 class EnvironmentSensor
     def self.get_data
         value = `python #{Rails.root}/lib/scripts/environment_sensor.py 0`.gsub("\n", '').to_i
-        print value
+        puts value.inspect
         return value if value == -1
     end
 end
