29810160c56e54d802f33ff319e9fbfc0806e2a6
Clément <clement.montois@ig2i.fr>
2017-05-19 22:35:39 +0200

    Some tests ...

--- lib/classes/environment_sensor.rb
+++ lib/classes/environment_sensor.rb
@@ -1,6 +1,6 @@
 class EnvironmentSensor
     def self.get_data
-        value = `python #{Rails.root}/lib/scripts/environment_sensor.py`
+        value = `python #{Rails.root}/lib/scripts/environment_sensor.py`.gsub("\n", '')
         value
     end
 end
