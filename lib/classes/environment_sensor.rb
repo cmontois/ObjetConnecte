fac71ab9ee4271be50a9633ea9365a05761e3f50
Clément <clement.montois@ig2i.fr>
2017-05-19 18:37:38 +0200

    -> Moved and rename BME280 lib

--- lib/classes/environment_sensor.rb
+++ lib/classes/environment_sensor.rb
@@ -1,6 +1,6 @@
 class EnvironmentSensor
     def self.get_data
-        value = `python #{Rails.root}/lib/scripts/BME280_I2C.py 0`.gsub("\n", '').to_i
+        value = `python #{Rails.root}/lib/scripts/environment_sensor.py 0`.gsub("\n", '').to_i
         return value if value == -1
     end
 end
