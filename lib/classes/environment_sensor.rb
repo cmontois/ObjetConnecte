4d0386e4f6e9921cbbead0a9236a82580c23c65c
Clément <clement.montois@ig2i.fr>
2017-05-19 22:16:32 +0200

    Some tests ...

--- lib/classes/environment_sensor.rb
+++ lib/classes/environment_sensor.rb
@@ -1,7 +1,6 @@
 class EnvironmentSensor
     def self.get_data
         value = `python #{Rails.root}/lib/scripts/environment_sensor.py`
-        puts value
-        return value if value == -1
+        value
     end
 end
